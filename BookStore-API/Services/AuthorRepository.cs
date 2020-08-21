7088d2af11e23c4ee5c846be0e5b05fdbfe91220
Fántics Attila <fantics.attila@windowslive.com>
2020-08-21 20:35:13 +0200

    Author repository

---
+++ BookStore-API/Services/AuthorRepository.cs
@@ -0,0 +1,51 @@
+﻿using BookStore_API.Contracts;
+using BookStore_API.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Services
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AuthorRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Create(Author entity)
+        {
+            await _db.Authors.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Author entity)
+        {
+            _db.Authors.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<IList<Author>> FindAll()
+        {
+            return await _db.Authors.ToListAsync();
+        }
+
+        public async Task<Author> FindById(int id)
+        {
+            return await _db.Authors.FindAsync(id);
+        }
+
+        public async Task<bool> Save()
+        {
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Update(Author entity)
+        {
+            _db.Authors.Update(entity);
+            return await Save();
+        }
+    }
+}
