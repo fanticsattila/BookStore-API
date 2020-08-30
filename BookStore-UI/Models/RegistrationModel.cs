ecda075a1fe224f24cd3e077a2327e5ef555ba9b
Fántics Attila <fantics.attila@windowslive.com>
2020-08-30 17:54:18 +0200

    Regisztrációs felület elkészítése Mindkét projekt futtatási módjának átállítása (IISExpress nem tud elindulni hiba miatt)

---
+++ BookStore-UI/Models/RegistrationModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore_UI.Models
+{
+    public class RegistrationModel
+    {
+        [EmailAddress]
+        [Display(Name ="Email address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(15, ErrorMessage = "Your password is limited to {2} to {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="The password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- BookStore-UI/Static/Endpoints.cs
+++ BookStore-UI/Static/Endpoints.cs
@@ -2,8 +2,9 @@
 {
     public static class Endpoints
     {
-        public static string BaseUrl = "https://localhost:44369/";
+        public static string BaseUrl = "https://localhost:5001";
         public static string AuthorsEndpoint = $"{BaseUrl}/api/authors/";
         public static string BooksEndpoint = $"{BaseUrl}/api/books/";
+        public static string RegisterEndpoint = $"{BaseUrl}/api/users/register";
     }
 }
